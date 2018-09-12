84dd8e3cae8b859e044048616689e67822fc4ef0
lacadindrei <lacadin.drei@gmail.com>
2018-09-12 16:45:07 +0800

    EOD september 12

---
+++ mod02-03/assets/js/script.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+    $("#cancelButton").click(function(){
+    	$("#text").val("");
+    });
+
+    // $("#btn-read").click(function(){
+    // 	alert("Wala pang laman! xD");
+    // })
+
+    $('#btn-read').click(function() {
+   	window.location = "https://www.apple.com/ph/iphone/"
+});
+});
